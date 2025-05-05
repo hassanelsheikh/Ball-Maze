900322b2ee961be2e828f9a6722036aa82837ca5
Hassan <htarek1011@gmail.com>
2025-05-05 19:50:38 +0300

    Solve background bug

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,3 +9,8 @@ func _on_play_pressed() -> void:
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
+	
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
